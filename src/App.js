f695a36f1131827d3a88dfb79c686172639b7663
Daniel Santos "Frank Deckard" Lopes Silva <71890228+MrCyberpunKx@users.noreply.github.com>
2021-03-16 11:46:13 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ListaDeSignos from './Componentes/ListaDeSignos';
+
+function App() {
+  return (
+    <ListaDeSignos />
+  );
+}
+
+export default App;
---
+++ src/Componentes/Titulo/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './estilo.css';
+
+function Titulo() {
+    return(
+        <div className="box">
+            <p className="texto">Baralho dos Signos(?)</p>
+
+        </div>
+    );
+}
+
+export default Titulo;
